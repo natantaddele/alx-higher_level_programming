17fd4a53c63646885d2311d0360c6dcd63a40cc9
natantaddele <50557762+natantaddele@users.noreply.github.com>
2023-03-08 01:50:46 +0300

    100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -6,12 +6,3 @@ i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(c - i)), end="")
     i = 32 if i == 0 else 0
-#!/usr/bin/python3
-# 101-remove_char_at.py
-
-
-def remove_char_at(str, n):
-    """Create a copy of the string without the character at position n."""
-    if n < 0:
-        return (str)
-    return (str[:n] + str[n+1:])
