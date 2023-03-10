2fcdcfea7b967afaa3a91bf742fb79979a3a7bfe
natantaddele <50557762+natantaddele@users.noreply.github.com>
2023-03-10 05:51:26 +0300

    Task-0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,11 +2,10 @@
 
 if __name__ == "__main__":
     """Print the sum of 1 and 2."""
-    import add_0
+    from add_0 import add
 
     a = 1
     b = 2
-    result = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
-    print(f"{a} + {b} = {result}\n")
 
