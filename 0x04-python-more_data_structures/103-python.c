fbe5569ee86db3125d032f4e05dcd036dcf21ffa
natantaddele <50557762+natantaddele@users.noreply.github.com>
2023-03-15 19:46:22 +0300

    Task-103

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,43 +1,48 @@
 #include <stdio.h>
 #include <Python.h>
 
-/**
- * print_python_bytes - Prints bytes information
- *
- * @p: Python Object
- * Return: no return
- */
+void print_python_list(PyObject *p)
+{
+    Py_ssize_t i, size;
+    PyObject *item;
+
+    if (!PyList_Check(p)) {
+        printf("  [ERROR] Invalid List Object\n");
+        return;
+    }
+
+    size = PyList_Size(p);
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+    for (i = 0; i < size; i++) {
+        item = PyList_GET_ITEM(p, i);
+        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+    }
+}
+
 void print_python_bytes(PyObject *p)
 {
-	char *string;
-	long int size, i, limit;
+    Py_ssize_t i, size;
+    char *str;
 
-	printf("[.] bytes object info\n");
-	if (!PyBytes_Check(p))
-	{
-		printf("  [ERROR] Invalid Bytes Object\n");
-		return;
-	}
+    printf("[.] bytes object info\n");
 
-	size = ((PyVarObject *)(p))->ob_size;
-	string = ((PyBytesObject *)p)->ob_sval;
+    if (!PyBytes_Check(p)) {
+        printf("  [ERROR] Invalid Bytes Object\n");
+        return;
+    }
 
-	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", string);
+    size = PyBytes_Size(p);
+    str = PyBytes_AsString(p);
 
-	if (size >= 10)
-		limit = 10;
-	else
-		limit = size + 1;
-
-	printf("  first %ld bytes:", limit);
-
-	for (i = 0; i < limit; i++)
-		if (string[i] >= 0)
-			printf(" %02x", string[i]);
-		else
-			printf(" %02x", 256 + string[i]);
-
-	printf("\n");
+    printf("  size: %ld\n", size);
+    printf("  trying string: %s\n", str);
+    printf("  first %ld bytes:", size + 1 > 10 ? 10 : size + 1);
+    for (i = 0; i < size + 1 && i < 10; i++)
+        printf(" %02hhx", str[i]);
+    printf("\n");
 }
+
 
