36c60eb5dbc01f003997e5903fd4cd6e3bdbfc35
natantaddele <50557762+natantaddele@users.noreply.github.com>
2023-03-15 19:15:49 +0300

    Task-7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:
-        del a_dictionary[key]
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key/value pairs in a dictionary."""
+    a_dictionary[key] = value
     return a_dictionary
-
