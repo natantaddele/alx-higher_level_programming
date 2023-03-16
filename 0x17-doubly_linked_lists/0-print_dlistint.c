8289f31895b6bb3bdeb5c4307c237d253c834e5e
natantaddele <50557762+natantaddele@users.noreply.github.com>
2023-03-16 22:11:07 +0300

    Task-0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,7 @@
-#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h
 
 /**
  * print_dlistint - Prints all the elements of a dlistint_t list
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,1 +1,18 @@
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Definition of the doubly-linked list node structure */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+/* Function prototype */
 size_t print_dlistint(const dlistint_t *h);
+
+#endif /* _LISTS_H_ */
