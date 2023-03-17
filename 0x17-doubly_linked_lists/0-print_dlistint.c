f448df1c01a63f55bec54b11d01ef0e0d1f9a227
natantaddele <50557762+natantaddele@users.noreply.github.com>
2023-03-17 22:33:09 +0300

    Task-0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,21 +10,8 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
  */
-typedef struct dlistint_s
-{
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
-} dlistint_t;
 
-/**
- * print_dlistint - prints all the elements of a dlistint_t list
- * @h: pointer to the head of the list
- *
- * Return: the number of nodes
- */
 size_t print_dlistint(const dlistint_t *h)
 {
     size_t count = 0;
