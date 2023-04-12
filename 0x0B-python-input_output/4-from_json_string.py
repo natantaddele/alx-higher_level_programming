ca7eb262495be668b6a0f3ed9f8c82b8545f2db9
natantaddele <50557762+natantaddele@users.noreply.github.com>
2023-04-12 22:39:22 +0300

    4. From JSON string to Object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 4-from_json_string
+Contains function that returns an object (Python data structure)
+represented by a JSON string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """Returns an object (Python data structure) represented by a JSON string"""
+    return json.loads(my_str)
