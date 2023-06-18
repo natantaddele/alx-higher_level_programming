9d51bd813a5651f94a82ee53d39037999128665b
natantaddele <50557762+natantaddele@users.noreply.github.com>
2023-06-18 20:57:46 +0300

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,43 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with\
-    N from the database hbtn_0e_0_usa.
-Usage: ./1-filter_states.py <mysql username> \
-                            <mysql password> \
-                            <database name>
-"""
+"""Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
-    )
-    c = db.cursor()
-    c.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
+    # Check if all required arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: ./script.py <mysql username> <mysql password> <database name>")
+        sys.exit(1)
+    
+    # Retrieve command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL server
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+        c = db.cursor()
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL:", e)
+        sys.exit(1)
+
+    # Execute the query to select states starting with N
+    try:
+        c.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        results = c.fetchall()
+        for state in results:
+            print(state)
+    except MySQLdb.Error as e:
+        print("Error executing MySQL query:", e)
+        sys.exit(1)
+
+    # Close the database connection
+    db.close()
