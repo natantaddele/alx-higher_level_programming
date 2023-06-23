f54f465c8752b19fd62b317e2087a00687bab9ee
natantaddele <50557762+natantaddele@users.noreply.github.com>
2023-06-23 21:36:04 +0300

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,46 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa
- Usage: ./0-select_states.py <mysql username>
-                            <mysql password>
-                            <database name >
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./script.py <mysql username> <mysql password> <database name>
 """
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+    # Check if all required arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: ./script.py <mysql username> <mysql password> <database name>")
+        sys.exit(1)
+
+    # Retrieve command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL server
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+        cursor = db.cursor()
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL:", e)
+        sys.exit(1)
+
+    # Execute the query to select all states ordered by id
+    try:
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        results = cursor.fetchall()
+        for state in results:
+            print(state)
+    except MySQLdb.Error as e:
+        print("Error executing MySQL query:", e)
+        sys.exit(1)
+
+    # Close the database connection
+    db.close()
